cb2504d4cc12a13bfcc2ef4d0213b300734bd136
Luciano Frias <lucianofrias1@hotmail.com>
2026-06-07 17:46:18 -0300

    First commit

---
+++ client/build/CMakeFiles/union_client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/union_client.dir/src/main.cpp.obj"
+  "CMakeFiles/union_client.dir/src/main.cpp.obj.d"
+  "libunion_client.dll.a"
+  "union_client.exe"
+  "union_client.exe.manifest"
+  "union_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/union_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client/src/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <winsock.h>
+#include <ws2tcpip.h>
+#include <tchar.h>
+#include <thread>
+
+#pragma comment (lib, "ws2_32.lib")
+
+bool initializeWinsockLib(){
+    WSADATA data;
+
+    return WSAStartup(MAKEWORD(2,2), &data) == 0;
+}
+
+int main(){
+
+    if (!initializeWinsockLib()){
+    std::cout << "Winsock lib failed to initialize" << std::endl;
+}
+
+SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+if (serverSocket == INVALID_SOCKET) {
+    std::cout << "Socket creation failed" << std::endl;
+}
+
+int port = 12345;
+std::string serverIPAddress = "127.0.0.1";
+sockaddr_in serverAddress;
+serverAddress.sin_family = AF_INET;
+serverAddress.sin_port = htons(port);
+inet_pton(AF_INET, serverIPAddress.c_str(), &(serverAddress.sin_addr));
+
+    if (connect(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR){
+        std::cout << "Not able to connect to server" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+
+        return 1;
+    }
+
+    std::cout << "Successfully connected to server" << std::endl;
+
+    std::thread senderThread = 
+
+
+    std::string message = "Hello there!";
+    int bytesSent = send(serverSocket, message.c_str(), message.length(), 0);
+
+    if (bytesSent == SOCKET_ERROR){
+        std::cout << "Message sent failed" << std::endl; 
+    }
+
+    closesocket(serverSocket);
+
+    WSACleanup();
+    return 0;
+}
