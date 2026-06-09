146bf035bfbcb7c403a8da16bf60035f6621082d
Luciano Frias <lucianofrias1@hotmail.com>
2026-06-08 21:13:47 -0300

    Added server folder

---
+++ server/build/CMakeFiles/union_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/union_server.dir/src/main.cpp.obj"
+  "CMakeFiles/union_server.dir/src/main.cpp.obj.d"
+  "libunion_server.dll.a"
+  "union_server.exe"
+  "union_server.exe.manifest"
+  "union_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/union_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
