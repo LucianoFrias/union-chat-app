31b4a08155801f038a3954b3a5c4ce50415df7fc
LucianoFrias <lucianofrias1@hotmail.com>
2026-07-01 15:00:29 -0300

    Added Google Test tests to CMake. Next tasks: Add every test for server and client, separate handleClient and handleServer into different small functions.

---
+++ server/tests/test_union_server.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../include/Server.h"
+
+TEST(UnionServerTest, IsOneOne)
+{
+    EXPECT_EQ(1, 1);
+}
+
+TEST(UnionServerTest, IsTwoTWo)
+{
+    EXPECT_EQ(2, 2);
+}
